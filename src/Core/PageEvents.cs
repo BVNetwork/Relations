9ebeb16ff383f7f722d438fb56cfa630562ae12f
Haakon Peder Haugsten <haakon@episerver.no>
2017-02-14 23:31:54 +0100

    Issue #4 - DeletedContent event throws System.NotImplementedException

--- src/Core/PageEvents.cs
+++ src/Core/PageEvents.cs
@@ -5,22 +5,22 @@ namespace EPiCode.Relations.Core
 {
     public class PageEvents : EPiServer.PlugIn.PlugInAttribute
     {
-        public static void Start() {
-            EPiServer.DataFactory.Instance.DeletedContent += Instance_DeletedContent;
+        public static void Start()
+        {
+            DataFactory.Instance.DeletedContent += Instance_DeletedContent;
         }
 
         private static void Instance_DeletedContent(object sender, DeleteContentEventArgs e)
         {
-            throw new System.NotImplementedException();
-        }
-
-        static void Instance_DeletedPage(object sender, DeleteContentEventArgs e)
-        {
-            if(e.Content != null && e.ContentLink != null) {
-                List<Relation> relationsToDelete = RelationEngine.Instance.GetRelationsForPage(e.ContentLink.ID);
-                foreach (Relation relation in relationsToDelete)
+            if (e.DeletedDescendents != null)
+            {
+                foreach (var deletedDescendents in e.DeletedDescendents)
                 {
-                    RelationEngine.Instance.DeleteRelation(relation);
+                    List<Relation> relationsToDelete = RelationEngine.Instance.GetRelationsForPage(deletedDescendents.ID);
+                    foreach (Relation relation in relationsToDelete)
+                    {
+                        RelationEngine.Instance.DeleteRelation(relation);
+                    }
                 }
             }
         }
